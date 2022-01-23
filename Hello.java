c069e8e55c0efec26fdbad4d29f54178ed80c791
CClemons11 <88004706+CClemons11@users.noreply.github.com>
2022-01-23 16:39:49 -0500

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/************************************************************
+* Hello.java
+*************************************************************
+* Christian Clemons
+* 1/23/22
+* CMSC 255 003
+*************************************************************/
+
+public class Hello {
+	public static void main (String [] args) {
+
+		System.out.println("<Christian Clemons>");
+		System.out.println("<Hello>");
+		System.out.println("<1/23/22>");
+		System.out.println("<CMSC 255 003>");
+		System.out.println();
+	}
+}
